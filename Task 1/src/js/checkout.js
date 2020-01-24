7b98d61ed57f96fa88c396ee8c642f53bfe51cc4
varunk19 <varunj6v1k9@gmail.com>
2020-01-24 11:24:07 +0530

    Remove directory

--- Task 1/src/js/checkout.js
+++
@@ -1,5 +0,0 @@
-console.log("checkout.js loaded");
-function buy () {
-    if(!document.getElementById("buy").disabled)
-        alert("Transaction status\nTotal: Rs."+gvars.total.toFixed(2)+"\nTransaction successful");
-}
--- Task 1/src/js/variables.js
+++
@@ -1,8 +0,0 @@
-console.log("variables.js loaded");
-let gvars = {
-    url: "src/assets/json/items.json",
-    qty: [],
-    mq: 0,
-    total: 0.00,
-    api: null
-};
